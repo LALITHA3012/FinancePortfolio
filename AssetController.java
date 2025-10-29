6b4614c892cab076d389bb9180ea67499b392ca0
LALITHA3012 <lalithasv06@gmail.com>
2025-10-29 15:53:40 +0530

    Create AssetController.java

---
+++ AssetController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Asset;
+import com.example.demo.service.AssetService;
+
+@RestController
+@RequestMapping("/api/assets")
+public class AssetController {
+
+    private final AssetService assetService;
+
+    public AssetController(AssetService assetService) {
+        this.assetService = assetService;
+    }
+
+    @GetMapping
+    public List<Asset> getAllAssets() {
+        return assetService.getAllAssets();
+    }
+
+    @PostMapping
+    public Asset addAsset(@RequestBody Asset asset) {
+        return assetService.saveAsset(asset);
+    }
+}
+
